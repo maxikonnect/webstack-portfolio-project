8207aa8683505c5f21fb25a1f4c2a9e695a7f83f
maxikonnect <tribillio@gmail.com>
2025-01-04 08:09:52 +0000

    latest news again

--- js/date.js
+++ js/date.js
@@ -2,4 +2,4 @@ const creditpara = document.querySelector(".credit-para");
 const creditspan =creditpara.querySelector(".credit-span");
 
 const date = new Date();
-creditspan.textContent = date.getFullYear();
+creditspan.textContent = " " + date.getFullYear() + " ";
