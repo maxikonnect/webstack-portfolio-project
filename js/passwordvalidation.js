4105836ae8bde1f2e8957c9e9d237651a05b48de
maxikonnect <tribillio@gmail.com>
2025-01-05 05:48:31 +0000

    password validity

--- js/passwordvalidation.js
+++ js/passwordvalidation.js
@@ -2,4 +2,19 @@ document.addEventListener(DOMContentLoaded, ()=>{
     const password = document.getElementById('your-password')
     const confirmPassword = document.getElementById('confirm-password')
     const form = document.querySelector('form');
+
+    confirmPassword.addEventListener('input', ()=>{
+        if(confirmPassword.value !== password.value){
+            confirmPassword.setCustomValidity('Passwords do not match.');
+        }else{
+            confirmPassword.setCustomValidity('');
+        }
+    });
+
+    form.addEventListener('submit', (event)=>{
+        if (confirmPassword.value !== passwordField.value) {
+            event.preventDefault();
+            alert('Passwords do not match. Please correct this before submitting.');
+        }
+    });
 })
