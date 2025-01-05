4861c35caff55095dd2ab21f8754762bc408628c
maxikonnect <tribillio@gmail.com>
2025-01-05 05:14:21 +0000

    validating password

---
+++ js/passwordvalidation.js
@@ -0,0 +1,5 @@
+document.addEventListener(DOMContentLoaded, ()=>{
+    const password = document.getElementById('your-password')
+    const confirmPassword = document.getElementById('confirm-password')
+    const form = document.querySelector('form');
+})
