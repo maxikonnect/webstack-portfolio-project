23d9f7464d9419f1270d4f08514885d547b5dc38
maxikonnect <tribillio@gmail.com>
2025-01-09 08:34:44 +0000

    javascript quiz being added

---
+++ js/tests/pstests/pstest1.js
@@ -0,0 +1,20 @@
+const quiz = {
+    JS:[
+        {
+            "id" : 1,
+            "question": "What is 2 * 2",
+            "options": {"a": "4", "b": "6", "c": "8", "d": "10"},
+            "answer":"4",
+            "score": 0,
+            "selectedOption": null,
+        },
+        {
+            "id": 2,
+            "question": "What is 4 * 2",
+            "options":{"a":"4", "b": "6", "c": "6", "d": "8"},
+            "answer":"8",
+            "score": 0,
+            "selectedOption": null,
+        }
+    ]
+}
